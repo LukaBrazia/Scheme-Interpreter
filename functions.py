f2b689c8eba87b822bbf0a9b6feb0ee29536a7a6
LukaBrazia <labsa20@freeuni.edu.ge>
2022-02-12 08:50:15 +0400

    Added lambda and define# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch master# Changes to be committed:#	modified:   functions.py#	modified:   interpreter.py#	modified:   test.scm## Untracked files:#	__pycache__/#

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
+
+
 class Function:
     def __init__(self, func):
         self.func = func
     def execute(self, params):
-        return self.func(self)
+        return self.func(params)
         
 def sub(args):
     if (len(args) == 0):
@@ -27,3 +29,85 @@ def div(args):
         return 1 / args[0]
     else:
         return args[0] / mult(args[1:])
+
+def andexp(args):
+    if (len(args) == 0):
+        return True
+    return args[0] and andexp(args[1:])
+
+def orexp(args):
+    if (len(args) == 0):
+        return False
+    return args[0] or andexp(args[1:])
+
+def car(args):
+    if len(args) != 1:
+        print("Incorrect number of arguements in call to car")
+        exit()
+    if not isinstance(args[0], list):
+        print("Arguement in call to car must be a list")
+        exit()
+    if args[0] == []:
+        print("Can't call car on an empty list")
+        exit()
+    return args[0][0]
+
+def cdr(args):
+    if len(args) != 1:
+        print("Incorrect number of arguements in call to cdr")
+        exit()
+    if not isinstance(args[0], list):
+        print("Arguement in call to cdr must be a list")
+        exit()
+    if args[0] == []:
+        print("Can't call cdr on an empty list")
+        exit()
+    return args[0][1:]
+
+def cons(args):
+    if len(args) != 2:
+        print("Incorrect number of arguements in call to cons")
+        exit()
+    if not isinstance(args[1], list):
+        print("Arguement in call to cons must be a list")
+        exit()
+    return [args[0]] + args[1]
+
+def append(args):
+    ret = []
+    for l in args:
+        ret += l
+    return ret
+
+def mapexp(args):
+    if len(args) < 2:
+        print("Incorrect number of arguements in call to map")
+        exit()
+    if not isinstance(args[0], Function):
+        print("Arguement in call to map not a function")
+        exit()
+    return [args[0].execute(list(t)) for t in list(zip(*args[1:]))]
+
+def apply(args):
+    if len(args) != 2:
+        print("Incorrect number of arguements in call to apply")
+        exit()
+    if not isinstance(args[0], Function):
+        print("Arguement in call to apply not a function")
+        exit()
+    if not isinstance(args[1], list):
+        print("Argument in call to apply has to be a list")
+        exit()
+    return args[0].execute(args[1])
+
+def eval(args):
+    if (len(args) != 1):
+        print("Incorrect number of arguments in call to eval")
+        exit()
+    if not isinstance(args[0], list):
+        print ("Argumnet to eval must be a list")
+        exit()
+    if not isinstance(args[0][0], Function):
+        print("Argument list to eval must start with a callable object")
+        exit()
+    return args[0][0].execute(args[0][1:])
--- interpreter.py
+++ interpreter.py
@@ -16,9 +16,9 @@ class Tokenizer:
         scanner = re.Scanner([
             ("\s", None), # Whitespace
             (";[^;\n]*", None), # Comments
-            ("\"[^\"]*\"|\'[^\"]*\'", valid("string")),
             ("'\s*\(", valid("raw_list")),
             ("\(", valid("list")),
+            ("\"[^\"]*\"|\'[^\"]*\'", valid("string")),
             ("\)", valid("endl")),
             ("-?[0-9]+", lambda _, tok : (int(tok), "int")),
             ("-?[0-9]+\.[0-9]*|-?[0-9]*\.[0-9]+", lambda _, tok : (float(tok), "float")),
@@ -42,8 +42,96 @@ class Interpreter:
             "-" : Function(functions.sub),
             "*" : Function(functions.mult),
             "/" : Function(functions.div),
-            ""
+            "and": Function(functions.andexp),
+            "or" : Function(functions.orexp),
+            "cdr": Function(functions.cdr),
+            "car": Function(functions.car),
+            "cons": Function(functions.cons),
+            "append": Function(functions.append),
+            "map": Function(functions.mapexp),
+            "apply" : Function(functions.apply),
+            "eval" : Function(functions.eval)
         }
+
+    def execute(self, type):
+        return self.exp_list()
+
+    def expect(self, st):
+        print("Was expecting a " + st)
+        exit()
+
+    def interpret_body(self, cli):
+        while self.token_index != len(tokens):
+            if self.peek()[1] == "raw_list":
+                self.next()
+                message = self.raw_list()
+                if cli and message is not None: print(message)
+            elif self.peek()[1] == "list":
+                self.next()
+                if self.peek()[0] == "define":
+                    self.next()
+                    if (self.peek()[1] == "list"):
+                        self.next()
+                        name = self.next()
+                        if name[1] != "id":
+                            print("Expected an id")
+                            exit()
+                        self.ids[name[0]] = Function(self.createFunc())
+                        self.next()
+                    else:
+                        self.createVar()
+                else:
+                    message = self.exp_list()
+                    if cli and message is not None: print(message)
+            else:
+                print(self.peek())
+                print("Expected a list")
+                exit()
+
+    def createVar(self):
+        if self.peek()[1] != "id":
+            print("Expected an ID")
+            exit()
+        name = self.next()[0]
+        val = self.get_expression()
+        if self.peek()[1] != "endl":
+            print("Expected a )")
+            exit()
+        self.next()
+        self.ids[name] = val
+
+
+    def createFunc(self):
+        params = {}
+        ind = 0
+        while self.peek()[1] != "endl":
+            if self.peek() == None:
+                print("Expected a )")
+                exit()
+            params[(self.next()[0])] = ind
+            ind += 1
+        self.next()
+        self.next() 
+        funcTokens = []
+        while self.peek()[1] != "endl":
+            if self.peek() == None:
+                print("Expected a )")
+                exit()
+            funcTokens.append(self.next())
+        funcTokens.append(self.next())
+        def func(args):
+            tempTokens = []
+            for tok in funcTokens:
+                if tok[0] in params.keys():
+                    tempTokens.append((args[params[tok[0]]], ""))
+                else:
+                    tempTokens.append(tok)
+            inp = Interpreter(tempTokens)
+            inp.ids = self.ids
+            return inp.exp_list()
+        return func
+            
+
     def next(self):
         if self.token_index == len(self.tokens):
             return None
@@ -60,13 +148,19 @@ class Interpreter:
 
     def get_expression(self):
         if self.peek()[1] == "list":
+            self.next()
             return self.exp_list()
         elif self.peek()[1] == "raw_list":
             self.next()
             return self.raw_list()
         elif self.peek()[1] == "id":
             if self.peek()[0] in self.ids.keys():
-                return self.ids[self.next[0]]
+                return self.ids[self.next()[0]]
+            elif self.peek()[0] == "lambda":
+                self.next()
+                self.next()
+                func = Function(self.createFunc())
+                return Function(lambda _ : func)
             else:
                 print(self.next[0] + " not defined")
                 exit()
@@ -75,7 +169,6 @@ class Interpreter:
         
 
     def exp_list(self):
-        self.next()
         func = self.get_expression()
         if not isinstance(func, Function):
             print("Object not callable")
@@ -111,5 +204,6 @@ if __name__ == "__main__":
         str = file.read()
         tk = Tokenizer(str)
         tokens = tk.get_tokens()
-        print(None == 1)
+        inp = Interpreter(tokens)
+        inp.interpret_body(True)
         file.close()
